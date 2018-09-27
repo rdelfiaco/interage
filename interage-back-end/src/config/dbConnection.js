86af15531cea758f88c1cb06b1237dd2e551c153
Ronaldo <ronaldo.delfiaco@ueg.br>
2018-09-27 11:02:12 -0300

    margem esquerda no login

--- interage-back-end/src/config/dbConnection.js
+++ interage-back-end/src/config/dbConnection.js
@@ -1,6 +1,6 @@
 module.exports = ({
-    // host: '192.168.100.28',
-    host: 'localhost',
+    host: '192.168.100.28',
+    //host: 'localhost',
     port: 5432,
     database: 'altis',
     user: 'postgres',
