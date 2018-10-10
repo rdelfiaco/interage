4b54f04f5f0ce6e0fa4505fbf2c414f2c26b24fa
Ronaldo <ronaldo.delfiaco@ueg.br>
2018-10-09 21:48:16 -0300

    formulário tarefas/eventos

--- interage-front-end/src/app/login/auth.service.ts
+++ interage-front-end/src/app/login/auth.service.ts
@@ -78,7 +78,7 @@ export class AuthService {
         token_access: usuarioLogado.token
       }
     })
-    debugger;
+    
     this.localStorage.delLocalStorage(`${usuarioLogado.token}_date`)
     this.localStorage.delLocalStorage('usuarioLogado_object')
     this.usuarioLogado.next(false);
