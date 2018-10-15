7db0bc051a840f08e21595eb54fe8b7268d9f6cd
José Augusto <joseneto.design@gmail.com>
2018-10-14 23:46:16 -0300

    Correção de Senha Pg

--- interage-back-end/src/config/dbConnection.js
+++ interage-back-end/src/config/dbConnection.js
@@ -2,6 +2,6 @@ module.exports = ({
     host: 'localhost',
     port: 5432,
     database: 'altis',
-    user: 'postgres',
-    password: 'postgres'
+    user: 'postegres',
+    password: 'altisinterage123!@#'
 });
