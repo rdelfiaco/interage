b132c3476371a1603cad1eddec8d95013e8eaafe
José Augusto <joseneto.design@gmail.com>
2018-10-15 23:45:15 -0300

    Servidor de Produção

--- interage-back-end/src/config/dbConnection.js
+++ interage-back-end/src/config/dbConnection.js
@@ -1,12 +1,12 @@
 //TROCA DADOS SERVIDOR
 
-// module.exports = ({ //treinamento
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'altis_teste',
-//     user: 'postgres',
-//     password: 'altisinterage123!@#'
-// });
+module.exports = ({ //treinamento
+    host: 'localhost',
+    port: 5432,
+    database: 'altis_teste',
+    user: 'postgres',
+    password: 'altisinterage123!@#'
+});
 
 
 // module.exports = ({ //Produção
@@ -18,10 +18,10 @@
 // });
 
 
-module.exports = ({ //localhost
-    host: '159.69.205.116',
-    port: 5432,
-    database: 'altis_teste',
-    user: 'postgres',
-    password: 'altisinterage123!@#'
-});
+// module.exports = ({ //localhost
+//     host: '159.69.205.116',
+//     port: 5432,
+//     database: 'altis_teste',
+//     user: 'postgres',
+//     password: 'altisinterage123!@#'
+// });
