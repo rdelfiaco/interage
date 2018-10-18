0604d888f5e09efda007fd216669742bb509edd3
José Augusto <joseneto.design@gmail.com>
2018-10-18 14:41:35 -0300

    conection db

--- interage-back-end/src/config/dbConnection.js
+++ interage-back-end/src/config/dbConnection.js
@@ -9,22 +9,19 @@
 // });
 
 
-module.exports = ({ //Produção
-    host: 'localhost',
+// module.exports = ({ //Produção
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'altis',
+//     user: 'postgres',
+//     password: 'altisinterage123!@#'
+// });
+
+
+module.exports = ({ //localhost
+    host: '159.69.205.116',
     port: 5432,
-    database: 'altis',
+    database: 'altis_teste',
     user: 'postgres',
     password: 'altisinterage123!@#'
 });
-
-
-// module.exports = ({ //localhost
-//     host: 'localhost',
-//     // host: '159.69.205.116',
-//     port: 5432,
-//     database: 'altis',
-//     // database: 'altis_teste',
-//     user: 'postgres',
-//     password: 'postgres'
-//     // password: 'altisinterage123!@#'
-// });
--- interage-back-end/src/config/nodeStart.js
+++ interage-back-end/src/config/nodeStart.js
@@ -1,11 +1,11 @@
 //TROCA DADOS SERVIDOR
-// module.exports =  ({ //treinamento
-//     port: 3010
-// })
-
-module.exports =  ({ //produção
-    port: 3000
+module.exports =  ({ //treinamento
+    port: 3010
 })
+
+// module.exports =  ({ //produção
+//     port: 3000
+// })
 
 // module.exports =  ({ //local
 //     port: 3010
