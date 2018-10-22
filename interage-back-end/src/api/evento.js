fcc6b8ac2e8d038a7b9bfe3f796d3193d34f8210
José Augusto <joseneto.design@gmail.com>
2018-10-22 15:33:55 -0300

    v.1.0.6

--- interage-back-end/src/api/evento.js
+++ interage-back-end/src/api/evento.js
@@ -13,13 +13,13 @@ function getUmEvento(req, res) {
       client.connect()
 
       let sql = `select *
-          from view_eventos
-          where id_status_evento in (1,4,5,6)
-          and ( (tipodestino = 'O' and id_pessoa_organograma = ${req.query.id_organograma} )
-            or  (tipodestino = 'P' and id_usuario = ${req.query.id_usuario} )
-            or  (id_pessoa_visualizou = ${req.query.id_pessoa} and id_status_evento in(5,6)  ))
-          and dt_para_exibir <= now()
-          order by id_status_evento desc, id_prioridade, dt_para_exibir LIMIT 1`
+            from view_eventos
+            where id_status_evento in (1,4,5,6)
+            and ( (tipodestino = 'O' and id_pessoa_visualizou is null and id_pessoa_organograma = ${req.query.id_organograma} )
+            or (tipodestino = 'P' and id_usuario = ${req.query.id_usuario} )
+            or (id_pessoa_visualizou = ${req.query.id_pessoa} and id_status_evento in(5,6) ))
+            and dt_para_exibir <= now()
+            order by id_status_evento desc, id_prioridade, dt_para_exibir LIMIT 1`
 
       console.log(sql)
       client.query(sql)
