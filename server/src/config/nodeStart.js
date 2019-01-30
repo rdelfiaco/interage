67509e646d9b1fcd77104be55c464fb166234406
José Augusto <joseneto.design@gmail.com>
2019-01-29 23:59:17 -0200

    v.1.0.17

--- server/src/config/nodeStart.js
+++ server/src/config/nodeStart.js
@@ -3,10 +3,10 @@
 //     port: 3010
 // })
 
-// module.exports =  ({ //produção
-//     port: 3000
-// })
-
-module.exports = ({ //local
-    port: 3010
+module.exports =  ({ //produção
+    port: 3000
 })
+
+// module.exports = ({ //local
+//     port: 3010
+// })
