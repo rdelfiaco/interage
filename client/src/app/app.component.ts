a3683d5cd6d4fac138635a7476b1848eb8c853a4
Ronaldo <ronaldo.delfiaco@ueg.br>
2019-02-14 08:26:36 -0200

    produção versão V.1.0.28

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   hasLogado: Observable<boolean>;
   usuarioLogado: Usuario;
   // TROCA DADOS SERVIDOR TROCAR NUMERO DA VERSÃO
-  versaoSistema: string = 'V.1.0.27';
+  versaoSistema: string = 'V.1.0.28';
   counterEvents: number;
   sub: any;
   constructor(private router: Router, private auth: AuthService, private connectHTTP: ConnectHTTP,
