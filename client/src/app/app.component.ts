ed9d62504b62656793a5ca47e22f07613f1947db
Ronaldo <ronaldo.delfiaco@ueg.br>
2019-03-07 10:57:50 -0300

    versào T.1.0.31

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   hasLogado: Observable<boolean>;
   usuarioLogado: Usuario;
   // TROCA DADOS SERVIDOR TROCAR NUMERO DA VERSÃO
-  versaoSistema: string = 'V.1.0.31';
+  versaoSistema: string = 'T.1.0.31';
   counterEvents: number;
   sub: any;
   constructor(private router: Router, private auth: AuthService, private connectHTTP: ConnectHTTP,
--- server/src/config/nodeStart.js
+++ server/src/config/nodeStart.js
@@ -1,12 +1,12 @@
  //TROCA DADOS SERVIDOR
 
-// module.exports =  ({ //treinamento
-//     port: 3010
-// })
-
-module.exports =  ({ //produção
-    port: 3000
+module.exports =  ({ //treinamento
+    port: 3010
 })
+
+// module.exports =  ({ //produção
+//     port: 3000
+// })
 
 // module.exports = ({ //local
 //     port: 3010
