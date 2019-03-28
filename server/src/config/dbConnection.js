6eac2f5f13660d14229946f2a542c0918c342618
Ronaldo <ronaldo.delfiaco@ueg.br>
2019-03-28 07:31:24 -0300

    v.1.0.34

--- server/src/config/dbConnection.js
+++ server/src/config/dbConnection.js
@@ -16,13 +16,13 @@
 //     password: 'altisinterage123!@#'
 // });
 
-module.exports = ({ //localhost
-    host: '192.168.100.28',
-    port: 5432,
-    database: 'altis',
-    user: 'postgres',
-    password: 'postgres'
-});
+// module.exports = ({ //localhost
+//     host: '192.168.100.28',
+//     port: 5432,
+//     database: 'altis',
+//     user: 'postgres',
+//     password: 'postgres'
+// });
 
 // module.exports = ({ //localhost
 //     host: 'localhost',
@@ -40,10 +40,10 @@ module.exports = ({ //localhost
 //     password: 'altisinterage123!@#'
 // });
 
-// module.exports = ({ //mac_producao 
-//     host: '159.69.205.116',
-//     port: 5432,
-//     database: 'altis',
-//     user: 'postgres',
-//     password: 'altisinterage123!@#'
-// });
+module.exports = ({ //mac_producao 
+    host: '159.69.205.116',
+    port: 5432,
+    database: 'altis',
+    user: 'postgres',
+    password: 'altisinterage123!@#'
+});
