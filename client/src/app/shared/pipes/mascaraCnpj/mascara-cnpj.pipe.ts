58a213f7316287775c14e6b37939604de5ce16de
Ronaldo <ronaldo.delfiaco@ueg.br>
2019-11-05 09:55:29 -0300

    pipe

--- client/src/app/shared/pipes/mascaraCnpj/mascara-cnpj.pipe.ts
+++ client/src/app/shared/pipes/mascaraCnpj/mascara-cnpj.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MascaraCnpjPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(v: any ): any {
+    debugger
+
+    if (v.length > 14) { v = v.substring(v.length - 14, v.length); }
+
+      v=v.replace(/\D/g,'') //Remove tudo o que não é dígito
+      //v=v.replace('/[^0-9]/', '')
+      v=v.replace(/(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)/,"$1$2.$3$4$5.$6$7$8/$9$10$11$12-$13$14") 
+
+      return v
+      }
+
 
 }
