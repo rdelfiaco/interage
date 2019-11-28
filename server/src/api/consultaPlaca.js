6034a4463918158fa74f6bdcd56ca09961d581b0
rdelfiaco <ronaldo.delfiaco@ueg.br>
2019-11-28 20:30:45 -0300

    ok

--- server/src/api/consultaPlaca.js
+++ server/src/api/consultaPlaca.js
@@ -1,12 +1,17 @@
 const { checkTokenAccess } = require('./checkTokenAccess');
-const sinesp = require('sinesp-nodejs')
+// const sinesp = require('sinesp-nodejs')
+const sinesp = require('sinesp-api')
 
 function consultarPlaca(req, res) {
-
+    console.log("consultarPlaca")
     return new Promise(function (resolve, reject) {
-
-        /* Realizar uma consulta contra a placa AAA-0001 */
-        sinesp.consultaPlaca(req.query.placa).then(dados => {
+        
+        sinesp.configure({
+            // proxy: {
+            //     host: "177.54.144.208",
+            //     port: "80",
+            // }
+        }).search(req.query.placa).then(dados => {
             resolve(dados);
         }).catch(err => {
             reject(err);
