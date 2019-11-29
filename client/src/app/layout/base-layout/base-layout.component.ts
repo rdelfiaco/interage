139c392a57133ebda75008d5575796eafd14d491
rdelfiaco <ronaldo.delfiaco@ueg.br>
2019-11-28 21:04:37 -0300

    V.2.0.19

--- client/src/app/layout/base-layout/base-layout.component.ts
+++ client/src/app/layout/base-layout/base-layout.component.ts
@@ -17,7 +17,7 @@ export class BaseLayoutComponent {
   usuarioLogado: Usuario;
   nomeUsuario: string = 'Usuário';
   // TROCA DADOS SERVIDOR TROCAR NUMERO DA VERSÃO
-  versaoSistema: string = 'T.2.0.19';
+  versaoSistema: string = 'V.2.0.19';
   counterEvents: number;
   sub: any;
   constructor(private router: Router, 
