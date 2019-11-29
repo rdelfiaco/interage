f3980e746e1387ed54cbe2c10dba05495607ee56
rdelfiaco <ronaldo.delfiaco@ueg.br>
2019-11-28 22:12:02 -0300

    consulta

--- server/src/api/consultaPlaca.js
+++ server/src/api/consultaPlaca.js
@@ -1,10 +1,10 @@
 const { checkTokenAccess } = require('./checkTokenAccess');
-const sinesp = require('sinesp-nodejs')
-// const sinesp = require('sinesp-api')
+// const sinesp = require('sinesp-nodejs')
+const sinesp = require('sinesp-api')
 
 function consultarPlaca(req, res) {
     return new Promise(function (resolve, reject) {
-        sinesp.consultaPlaca(req.query.placa).then(dados => {
+        sinesp.search(req.query.placa).then(dados => {
             resolve(dados);
         }).catch(err => {
             reject(err);
