2f3e95debde2a807ebe0719df44d4aa5453fd083
rdelfiaco <ronaldo.delfiaco@ueg.br>
2019-11-28 22:34:11 -0300

    git push

--- server/src/config/dbConnection.js
+++ server/src/config/dbConnection.js
@@ -1,12 +1,12 @@
 //TROCA DADOS SERVIDOR
 
-// module.exports = ({ //treinamento
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'altis_teste',
-//     user: 'postgres',
-//     password: 'altisinterage123!@#'
-// });
+module.exports = ({ //treinamento
+    host: 'localhost',
+    port: 5432,
+    database: 'altis_teste',
+    user: 'postgres',
+    password: 'altisinterage123!@#'
+});
 
 // module.exports = ({ //Produção
 //     host: 'localhost',
@@ -24,13 +24,13 @@
 //     password: 'postgres'
 // });
 
-module.exports = ({ //localhost
-    host: 'localhost',
-    port: 5432,
-    database: 'altis',
-    user: 'postgres',
-    password: 'postgres'
-});
+// module.exports = ({ //localhost
+//     host: '192.168.100.161',
+//     port: 5432,
+//     database: 'altis',
+//     user: 'postgres',
+//     password: 'postgres'
+// });
 
 // module.exports = ({ //treinamento
 //     host: '159.69.205.116',
