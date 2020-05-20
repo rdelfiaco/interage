8f6fb2636ea562aa447a0b1e79e3eadd673ec540
Ronaldo <ronaldo.delfiaco@ueg.br>
2020-05-20 13:03:48 -0300

    correção

--- server/src/api/consultaPlaca.js
+++ server/src/api/consultaPlaca.js
@@ -1,25 +1,26 @@
 const { checkTokenAccess } = require('./checkTokenAccess');
 // const sinesp = require('sinesp-nodejs')
-const sinesp = new require('sinesp-api')
+//const sinesp = new require('sinesp-api')
 var request = require('request');
 
 async function consultarPlaca(req, res) {
 
-    await sinesp.configure(
-        timeout= 0,
-        host= 'cidadao.sinesp.gov.br',
-        endpoint= '/sinesp-cidadao/mobile/consultar-placa/',
-        serviceVersion= 'v5',
-        androidVersion= '6.0',
-        secret= '0KnlVSWHxOih3zKXBWlo',
-        maximumRetry= 3,
-        proxy= {
-            host: '187.63.111.37' ,
-            port: '3128'
-            }
-        )  
-    let dadosPlaca = await sinesp.search(req.query.placa) 
+    // await sinesp.configure(
+    //     timeout= 0,
+    //     host= 'cidadao.sinesp.gov.br',
+    //     endpoint= '/sinesp-cidadao/mobile/consultar-placa/',
+    //     serviceVersion= 'v5',
+    //     androidVersion= '6.0',
+    //     secret= '0KnlVSWHxOih3zKXBWlo',
+    //     maximumRetry= 3,
+    //     proxy= {
+    //         host: '187.63.111.37' ,
+    //         port: '3128'
+    //         }
+    //     )  
+    // let dadosPlaca = await sinesp.search(req.query.placa) 
 
+    let dadosPlaca = ''
 
      buscaTabelaFipe(dadosPlaca)
 
